9684710a6075cbf8f50488ff716304306c5e486a
feditech <82119072+feditech@users.noreply.github.com>
2022-12-07 02:45:44 +0500

    map installed

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { View} from 'react-native';
 import Login from './src/Screen/Login';
+import Map from './src/Screen/Map';
 import Signup from './src/Screen/Signup';
 
 const App = () => {
   return (
-      <Signup />
+      // <Signup />
       // <Login />
+      <Map />
   );
 }; 
 
